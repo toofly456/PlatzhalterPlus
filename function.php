95718f9dc44b294735045a15644b4d78ecdd7129
toofly456 <91848866+toofly456@users.noreply.github.com>
2023-07-06 00:17:27 +0200

    Update function.php

--- function.php
+++ function.php
@@ -6,6 +6,7 @@ function NoHome($pdo, $user_id)
 {
     $stmt = $pdo->prepare("SELECT COUNT(rolle_id) FROM role WHERE uid = ? AND rolle_id = '3' AND active = '1'");
     $stmt->execute([$user_id]);
+    //einmal ausgeben
     return $stmt->fetchColumn();
 }
 
@@ -28,6 +29,7 @@ function isContentCreator($pdo, $user_id)
 
 function getSqlCommands($pdo, $user_id)
 {
+    #visible = 1 meint, dass die SQLs NICHT angezeigt werden!
     $stmt = $pdo->prepare("SELECT befehl FROM sql_code WHERE user_id = ? AND visible != 1");
     $stmt->execute([$user_id]);
     return $stmt->fetchAll(PDO::FETCH_COLUMN);
@@ -54,9 +56,6 @@ function getSqltitle2($pdo, $user_id, $id)
 }
 
 
-
-
-
 function Code(PDO $pdo, $id)
 {
     $stmt = $pdo->prepare("SELECT code FROM users WHERE id = ?");
@@ -64,25 +63,19 @@ function Code(PDO $pdo, $id)
     return $stmt->fetchAll(PDO::FETCH_COLUMN);
 }
 
-function CodeUid(PDO $pdo, $code)
-{
-    $stmt = $pdo->prepare("SELECT id FROM users WHERE code = '?'");
-    $stmt->execute([$code]);
-    return $stmt->fetchAll(PDO::FETCH_COLUMN);
-}
-
 function getUSERcode($pdo, $user_id)
 {
-    $stmt = $pdo->prepare("SELECT u.id, u.vorname, u.nachname, u.code FROM users u INNER JOIN orga o ON o.uid = u.id WHERE o.vorgesetzter = ? ORDER BY u.id DESC;");
+    $stmt = $pdo->prepare("SELECT u.id, u.vorname, u.nachname, u.code FROM users u INNER JOIN orga o ON o.uid = u.id WHERE o.vorgesetzter = ? AND u.active = 1 ORDER BY u.id DESC;");
     $stmt->execute([$user_id]);
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function getEmail($pdo, $benutzer)
 {
-  $stmt = $pdo->prepare("SELECT email FROM users WHERE id = ?");
-  $stmt->execute([$benutzer]);
-  return $stmt->fetchColumn();
+    $stmt = $pdo->prepare("SELECT email FROM users WHERE id = ?");
+    $stmt->execute([$benutzer]);
+    //einmal ausgeben
+    return $stmt->fetchColumn();
 }
 
 
@@ -114,7 +107,7 @@ function searchForKeywords($item, $pdo)
                             <table>
                                 <tr>
                                     <td>
-                                        <input id="text" name="<?php echo strtolower($Platzhalterx) ?>" value="" minlength="1" maxlength="100" <?php if (isset($_POST['menu']) == false) { ?> required <?php } ?> style="width:auto;"></input>
+                                        <input id="text" name="<?php echo strtolower($Platzhalterx) ?>" value="" minlength="1" maxlength="100" <?php if (isset($_POST['menu']) == false) { ?> required <?php } ?> style="width:80%;"></input>
 
                                     </td>
                                     <td>
@@ -130,7 +123,7 @@ function searchForKeywords($item, $pdo)
                                 <tr>
                                     <td>
                                         <input id="text" name="<?php echo $Platzhalterx ?>" size="70" value="" minlength="1" maxlength="100" <?php
-                                                                                                                                                if (isset($_POST['menu']) == false) { ?> required <?php } ?> style="width:auto;"></input>
+                                                                                                                                                if (isset($_POST['menu']) == false) { ?> required <?php } ?> style="width:80%;"></input>
 
                                     </td>
                                     <td>
@@ -139,80 +132,49 @@ function searchForKeywords($item, $pdo)
                                 </tr>
                             </table>
 
-                        <?php
+                    <?php
                         }
                     }
                 }
             }
         }
     }
-    // ungebraucht (Großschreibung)
+
+
     foreach ($words as $word) {
         foreach ($keywords as $keyword) {
-            if (strpos($word, ucfirst($keyword)) !== false) {
 
-                $stmt = $pdo->prepare("SELECT CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) FROM platzhalter WHERE user_id = ? AND (name = 'Passwort' OR name = 'Datum')");
-                $stmt->execute([$id]);
-                $platzhalter2 = $stmt->fetchAll(PDO::FETCH_COLUMN);
+            //datum / passwort Kleinschreibung
+            $stmt = $pdo->prepare("SELECT name FROM platzhalter WHERE user_id = ? AND  (name = 'passwort' OR name LIKE '%datum%');");
+            $stmt->execute([$id]);
+            $platzhalter3 = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
-                foreach ($platzhalter2 as $Platzhalterx2) {
-                    if (strpos($word, ucfirst($Platzhalterx2)) !== false && !in_array($Platzhalterx2, $usedPlatzhalter) && (($Platzhalterx2 === 'Passwort' && !in_array('Datum', $usedPlatzhalter)) || ($Platzhalterx2 === 'Datum' && !in_array('Passwort', $usedPlatzhalter)))) {
-                        array_push($usedPlatzhalter, $Platzhalterx2);
-                        ?>
-                        <table>
-                            <tr>
-                                <td>
-                                    <input name="<?php echo $Platzhalterx2 ?>" size="70" <?php
-                                                                                            if ($Platzhalterx2 === 'Passwort') {
-                                                                                                echo 'type="text" value="' . $passwort . '"';
-                                                                                            } else if ($Platzhalterx2 === 'Datum') {
-                                                                                                echo 'type="date"';
-                                                                                            } ?>></input>
-                                </td>
-                                <td>
-                                    <input id="<?php echo "[" . $Platzhalterx2 . "]" ?>" value="<?php echo $Platzhalterx2 ?>" size="30" readonly="readonly"></input>
-                                </td>
-                            </tr>
-                        </table>
-
-                    <?php
-                    }
-                }
-            } else {
-
-
-                //datum / passwort Kleinschreibung
-                $stmt = $pdo->prepare("SELECT name FROM platzhalter WHERE user_id = ? AND  (name = 'passwort' OR name LIKE '%datum%');");
-                $stmt->execute([$id]);
-                $platzhalter3 = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-                foreach ($platzhalter3 as $Platzhalterx3) {
-                    if (strpos($word, strtolower($Platzhalterx3)) !== false && !in_array($Platzhalterx3, $usedPlatzhalter)) {
-                        array_push($usedPlatzhalter, $Platzhalterx3);
-                        // Array mit ähnlichen Begriffen
-                        $aehnlicheBegriffe = array('aenderung_datum', 'datum');
+            foreach ($platzhalter3 as $Platzhalterx3) {
+                if (strpos($word, strtolower($Platzhalterx3)) !== false && !in_array($Platzhalterx3, $usedPlatzhalter)) {
+                    array_push($usedPlatzhalter, $Platzhalterx3);
+                    // Array mit ähnlichen Begriffen
+                    $aehnlicheBegriffe = array('aenderung_datum', 'datum');
                     ?>
-                        <table>
-                            <tr>
-                                <td>
-                                    <input name="<?php echo $Platzhalterx3 ?>" size="70" <?php
-                                                                                            if ($Platzhalterx3 === 'passwort') {
-                                                                                                echo 'type="text" value="' . $passwort . '"';
-                                                                                            } else if (in_array($Platzhalterx3, $aehnlicheBegriffe)) {
-                                                                                                $currentDateTime = date('Y-m-d'); // Aktuelles Datum und Uhrzeit
-                                                                                                echo 'type="date" value="' . $currentDateTime . '"';
-                                                                                            } ?> style="width:auto;">
-                                    </input>
+                    <table>
+                        <tr>
+                            <td>
+                                <input name="<?php echo $Platzhalterx3 ?>" size="70" <?php
+                                                                                        if ($Platzhalterx3 === 'passwort') {
+                                                                                            echo 'type="text" value="' . $passwort . '"';
+                                                                                        } else if (in_array($Platzhalterx3, $aehnlicheBegriffe)) {
+                                                                                            $currentDateTime = date('Y-m-d'); // Aktuelles Datum und Uhrzeit
+                                                                                            echo 'type="date" value="' . $currentDateTime . '"';
+                                                                                        } ?> style="width:auto;">
+                                </input>
 
-                                </td>
-                                <td>
-                                    <input id="<?php echo "[" . $Platzhalterx3 . "]" ?>" value="<?php echo $Platzhalterx3 ?>" size="30" readonly="readonly" style="width:auto;"></input>
-                                </td>
-                            </tr>
-                        </table>
+                            </td>
+                            <td>
+                                <input id="<?php echo "[" . $Platzhalterx3 . "]" ?>" value="<?php echo $Platzhalterx3 ?>" size="30" readonly="readonly" style="width:auto;"></input>
+                            </td>
+                        </tr>
+                    </table>
 
 <?php
-                    }
                 }
             }
         }
