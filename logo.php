7e34b46fe28c07adbf8d1992b4b187cb2c1b97ca
toofly456 <91848866+toofly456@users.noreply.github.com>
2023-07-06 02:43:08 +0200

    Update logo.php

--- logo.php
+++ logo.php
@@ -4,14 +4,39 @@ include "db_conn.php";
 $stmt = $pdo->prepare("SELECT bild FROM bilderpool WHERE id = 1 AND active = 1");
 $stmt->execute();
 $image = $stmt->fetch()['bild'];
-
 $imageData = base64_encode($image);
-
 
 
 $stmt = $pdo->prepare("SELECT bild FROM bilderpool WHERE id = 4 AND active = 1");
 $stmt->execute();
 $image = $stmt->fetch()['bild'];
-
 $imageDataProfil = base64_encode($image);
+
+
+
+
+$stmt = $pdo->prepare("SELECT bild FROM bilderpool WHERE id = 7 AND active = 1");
+$stmt->execute();
+$image = $stmt->fetch()['bild'];
+$imageDataGif1 = base64_encode($image);
+
+$stmt = $pdo->prepare("SELECT bild FROM bilderpool WHERE id = 8 AND active = 1");
+$stmt->execute();
+$image = $stmt->fetch()['bild'];
+$imageDataGif2 = base64_encode($image);
+
+$stmt = $pdo->prepare("SELECT bild FROM bilderpool WHERE id = 9 AND active = 1");
+$stmt->execute();
+$image = $stmt->fetch()['bild'];
+$imageDataGif3 = base64_encode($image);
+
+$stmt = $pdo->prepare("SELECT bild FROM bilderpool WHERE id = 10 AND active = 1");
+$stmt->execute();
+$image = $stmt->fetch()['bild'];
+$imageDataGif4 = base64_encode($image);
+
+$stmt = $pdo->prepare("SELECT bild FROM bilderpool WHERE id = 11 AND active = 1");
+$stmt->execute();
+$image = $stmt->fetch()['bild'];
+$imageDataGif5 = base64_encode($image);
 ?> 
