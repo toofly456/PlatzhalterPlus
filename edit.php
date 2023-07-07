6b8d7bb713ad4ebcca26716104a849527e3d4f26
toofly456 <91848866+toofly456@users.noreply.github.com>
2023-07-08 00:03:52 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,6 +4,10 @@
 <head>
     <title>PlatzhalterPlus</title>
     <link rel="stylesheet" type="text/css" href="bootstrap.css" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
